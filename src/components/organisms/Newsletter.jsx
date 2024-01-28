6273f850ac0c87aefa6c6df34f5d8d411e7bdd39
owen <mowen8983@gmail.com>
2024-01-28 10:06:42 +0700

    feat(Newsletter): Adding newsletter section

---
+++ src/components/organisms/Newsletter.jsx
@@ -0,0 +1,17 @@
+const Newsletter = () => {
+  return (
+    <>
+      <div className='md:flex md:justify-center md:items-center bg-white py-12 h-[50rem]'>
+        <div className='w-full flex justify-center md:w-1/2'>
+          <h1>Want tips & tricks to optimize your flow?</h1>
+          <p>Sign up to our newsletter and stay up to date</p>
+        </div>
+        <div className='w-full md:w-1/2 p-2 flex flex-col'>
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Newsletter;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import Navbar from './components/organisms/Navbar';
 import './index.css';
 import HeroSection from './components/organisms/Hero';
 import About from './components/organisms/About';
+import Newsletter from './components/organisms/Newsletter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <HeroSection />
     <About />
+    <Newsletter />
   </React.StrictMode>
 );
